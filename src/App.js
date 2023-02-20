aea6a115351e89fca15f9a62a665522f099c803f
JustinasKoncius <justinas.koncius@ktu.edu>
2023-02-20 22:07:52 +0200

    Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App() {
-  return null;
+
+  return (
+  <div style={{
+    fontWeight:'Bold',
+    fontSize:'125px',
+    top:'40%',
+    left:'25%',
+    position:'absolute',
+    color:'red'
+    }}>
+    Kur home page? xd
+  </div>
+  );
 }
 
 export default App;
