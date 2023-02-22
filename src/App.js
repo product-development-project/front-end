e9cf3ce7801f63786e5419a01f858abdd0a5772d
Marijus Petkevičius <ma.petkevicius@ktu.edu>
2023-02-22 18:10:24 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import React from "react";
 function App() {
 
   return (
-  <div style={{
-    fontWeight:'Bold',
-    fontSize:'125px',
-    top:'40%',
-    left:'25%',
-    position:'absolute',
-    color:'red'
-    }}>
-    Kur home page? xd
-  </div>
+    null
   );
 }
 
