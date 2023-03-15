dbc7cb3cc2e19737f04fef26dd15cb8074fcc0c7
Marijus Petkevičius <ma.petkevicius@ktu.edu>
2023-03-16 01:29:33 +0200

    Revert to to correct commit

--- src/components/JobAds/JobAdsListPage.jsx
+++ src/components/JobAds/JobAdsListPage.jsx
@@ -1,4 +1,5 @@
 import TopBar from '../TopBar';
+import React, { useState, useEffect } from 'react'
 import { Button } from '../UI/Button';
 import Footer from '../Footer';
 import { useNavigate } from 'react-router-dom';
