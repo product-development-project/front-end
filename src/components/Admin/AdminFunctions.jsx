ef77aaa99c1baeefe0022139e1f0da13be7f4111
augis1123 <99415938+augis1123@users.noreply.github.com>
2023-05-25 18:31:32 +0300

    s

--- src/components/Admin/AdminFunctions.jsx
+++ src/components/Admin/AdminFunctions.jsx
@@ -1,58 +1,87 @@
-import Header from '../Header';
-import { Button } from '../UI/Button';
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import './style.css';
+import React from "react";
+import Header from "../Header";
+import { Button, Box } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 export default function AdminFunctionsFunction() {
   const navigate = useNavigate();
 
   return (
     <>
-      <Header></Header>
+      <Header />
 
-      <div className="DivBox delayed-animation" style={{ animationDelay: `${2 * 50}ms` }}>
-        <Button
-          value="Approve tasks"
-          className="delayed-animation"
-          name="company-button"
-          onClick={() => navigate('/home/Admin/ApproveTasks')}
-          style={{
-            marginTop: '15px',
-            animationDelay: `${6 * 50}ms`
-          }}
-        />
-        <Button
-          value="Approve companies"
-          className="delayed-animation"
-          name="company-button"
-          onClick={() => navigate('/home/Admin/ApproveCompanies')}
-          style={{
-            marginTop: '15px',
-            animationDelay: `${8 * 50}ms`
-          }}
-        />
-        <Button
-          value="Create tasks"
-          className="delayed-animation"
-          name="company-button"
-          onClick={() => navigate('/home/Admin/CreateTask')}
-          style={{
-            marginTop: '15px',
-            animationDelay: `${10 * 50}ms`
-          }}
-        />
-        <Button
-          value="Help section"
-          className="delayed-animation"
-          name="company-button"
-          onClick={() => navigate('/home/Admin/HelpSection')}
-          style={{
-            marginTop: '15px',
-            animationDelay: `${15 * 50}ms`
-          }}
-        />
+      <div
+        className="DivBox delayed-animation"
+        style={{ animationDelay: `${2 * 50}ms` }}
+      >
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Box mb={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => navigate("/home/Admin/ApproveTasks")}
+              style={{
+                marginTop: "15px",
+                animationDelay: `${6 * 50}ms`,
+                width: "200px",
+                background: "white",
+                border: "2px solid blue",
+              }}
+            >
+              <span style={{ color: "black" }}>Approve Tasks</span>
+            </Button>
+          </Box>
+          <Box mb={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => navigate("/home/Admin/ApproveCompanies")}
+              style={{
+                marginTop: "15px",
+                animationDelay: `${8 * 50}ms`,
+                width: "200px",
+                background: "white",
+                border: "2px solid blue",
+              }}
+            >
+              <span style={{ color: "black" }}>Approve Companies</span>
+            </Button>
+          </Box>
+          <Box mb={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => navigate("/home/Admin/CreateTask")}
+              style={{
+                marginTop: "15px",
+                animationDelay: `${10 * 50}ms`,
+                width: "200px",
+                background: "white",
+                border: "2px solid blue",
+              }}
+            >
+              <span style={{ color: "black" }}>Create Task</span>
+            </Button>
+          </Box>
+          <Box mb={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => navigate("/home/Admin/HelpSection")}
+              style={{
+                marginTop: "15px",
+                animationDelay: `${15 * 50}ms`,
+                width: "200px",
+                background: "white",
+                border: "2px solid blue",
+              }}
+            >
+              <span style={{ color: "black" }}>Help Section</span>
+            </Button>
+          </Box>
+        </Box>
       </div>
     </>
   );
-};
+}
